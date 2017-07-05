eebc680f62a5310b8bbf6bb005eeaebd8b11ac82
Nikita Ashikhmin <veoring@gmail.com>
2017-07-05 18:01:15 +0300

    removed colors

--- src/main/resources/terraform/terraform_run.sh
+++ src/main/resources/terraform/terraform_run.sh
@@ -9,5 +9,5 @@ if [ -z "$MY_PATH" ]; then
     exit 1 # fail
 fi
 cd ${MY_PATH}
-terraform plan -out hello-world-config.tfplan
-terraform apply
+terraform plan -out  -no-color hello-world-config.tfplan
+terraform apply  -no-color
