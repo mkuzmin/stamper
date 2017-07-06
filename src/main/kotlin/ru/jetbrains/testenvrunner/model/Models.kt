f24e59bfadbfc27b395e66c6eb04e01d8d549ee0
Nikita Ashikhmin <veoring@gmail.com>
2017-07-06 12:58:48 +0300

    small refactor of ExecutionResult class

--- src/main/kotlin/ru/jetbrains/testenvrunner/model/Models.kt
+++ src/main/kotlin/ru/jetbrains/testenvrunner/model/Models.kt
@@ -2,4 +2,4 @@ package ru.jetbrains.testenvrunner.model
 
 data class ExecutionCommand(var command: String = "")
 
-data class ExecutionResult(val result: String = "", val exitValue: Int = -1)
+data class ExecutionResult(val output: String = "", val exitValue: Int = -1)
--- src/test/kotlin/ru/jetbrains/testenvrunner/utils/BashExecuterTest.kt
+++ src/test/kotlin/ru/jetbrains/testenvrunner/utils/BashExecuterTest.kt
@@ -12,7 +12,7 @@ class BashExecuterTest : Assert() {
     fun runEchoHelloTest() {
         val value = "hello, world!"
         val result = BashExecutor.executeCommand(ExecutionCommand("echo $value"))
-        assertEquals(MSG_OUTPUT, value + "\n", result.result)
+        assertEquals(MSG_OUTPUT, value + "\n", result.output)
         assertTrue(MSG_EXIT_CODE, result.exitValue == 0)
     }
 
@@ -21,7 +21,7 @@ class BashExecuterTest : Assert() {
         val value = "ping google.com"
 
         val result = BashExecutor.executeCommand(ExecutionCommand(value), waitingTime = 1)
-        assertEquals(MSG_OUTPUT, BashExecutor.MSG_TIMEOUT_ERROR_BASH.format(value), result.result)
+        assertEquals(MSG_OUTPUT, BashExecutor.MSG_TIMEOUT_ERROR_BASH.format(value), result.output)
         assertTrue(MSG_EXIT_CODE, result.exitValue != 0)
     }
 
@@ -29,7 +29,7 @@ class BashExecuterTest : Assert() {
     fun runNonexistentCommandTest() {
         val value = "abrakadabra"
         val result = BashExecutor.executeCommand(ExecutionCommand(value), waitingTime = 1)
-        assertEquals(MSG_OUTPUT, "Cannot run program \"$value\": error=2, No such file or directory", result.result)
+        assertEquals(MSG_OUTPUT, "Cannot run program \"$value\": error=2, No such file or directory", result.output)
         assertTrue(MSG_EXIT_CODE, result.exitValue != 0)
     }
 }
